9729188f2c1f9ff44d8ab06fbd804c97714e1a23
natipro22 <natipro22@github.com>
2023-04-04 13:19:26 +0300

    Initial commit

---
+++ JWTAuthenitcation/Controllers/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTAuthenitcation.Controllers
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = "";
+        public string EmailAddress { get; set; } = "";
+    }
+}
